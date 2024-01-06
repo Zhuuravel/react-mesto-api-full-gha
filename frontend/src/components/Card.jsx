e7ee371ff368e3d73c5d59c15a531a90609b528a
Zhuravel <zhuuravel@yandex.ru>
2024-01-06 16:55:45 +0300

    fixed cards back

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card({card, onCardClick, onCardLike, onCardDelete}) {
 
     const currentUser = useContext(CurrentUserContext);
     // Определяем, являемся ли мы владельцем текущей карточки
-    const isOwn = card.owner === currentUser._id;
+    const isOwn = card.owner._id === currentUser._id;
 
     // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
     const isLiked = card.likes.some(i => i === currentUser._id);
