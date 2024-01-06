22937dc500aa0b4def9e48082ad8ec5eb791fc5c
Zhuravel <zhuuravel@yandex.ru>
2024-01-06 17:22:32 +0300

    fixed cards 2

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -19,7 +19,7 @@ module.exports.getCards = (req, res, next) => {
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
-  Card.create({ name, link, owner: req.user._id })
+  Card.create({ name, link, owner })
     .then((card) => {
       Card.findById(card._id)
         .populate('owner')
